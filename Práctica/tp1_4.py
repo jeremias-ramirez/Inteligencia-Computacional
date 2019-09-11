b0b20b06725be9752045525b1a9326d78e2d2460
Jeremias <jeremiaslagorio@gmail.com>
2019-09-11 17:22:26 -0300

    tp1_4

--- Práctica/tp1_4.py
+++ Práctica/tp1_4.py
@@ -21,21 +21,26 @@ trn = np.append(-np.ones((len(data[:,1]),1)), data[:, 0:4],1)
 np.random.seed(19680801)
 # inicializo aleatoriamente los pesos W - vector de entradas y vector con la cantidad de neuronas
 
-w = initW.initialize_w( np.ones((5,1)), np.array([8,4,3], np.int ))
+w = initW.initialize_w( np.ones((5,1)), np.array([8,3], np.int ))
 
 vel = 0.1
+velM = 0.4
 
-epoc = 500
-
-accurV = np.zeros((epoc,1))
-errorV = np.zeros((len(trn[:,1]),epoc))
+epoc = 50
 
 tasa = 0.8
 
+accurV = np.zeros((epoc,1))
 errorCV = np.zeros((epoc,1))
 
 k = 10
+
 H, W = data.shape
+
+cantPart = int(H/k)
+accurVPar = np.zeros((cantPart,1))
+errorVPar = np.zeros((cantPart,1))
+
 indexs = np.arange(0, H, 1)
 #vPrueba = np.zeros((int(H/k),1))
 #vTrain = np.zeros((int(H/k),1))
@@ -49,7 +54,7 @@ for i in range(int(H/k)):
     yd = np.expand_dims(dataTrain[:, 4::], axis = 1)
 
     for e in range(epoc):
-
+        deltaW = None
         for j in range(len(trn[:,0])):
             inputV = np.expand_dims(trn[j,:], axis = 1)
 #            inputV = np.expand_dims(trn[j,:], axis=1)
@@ -58,7 +63,7 @@ for i in range(int(H/k)):
 #                print(ys.shape)
 #            print(y1) 
 
-            w = bp.backpropagation(w, y1, yd[j, :].T, vel)
+            w,deltaW = bp.backpropagation_momento(w, y1, yd[j, :].T, vel, velM, deltaW)
    
         accur = 0
         errorC = 0
@@ -68,19 +73,23 @@ for i in range(int(H/k)):
             y = salY.salidasy(inputV, w)
         
             ye = yd[j, :].T
-            ysalida1 = 1 if y[-1][-3] > 0 else -1
-            ysalida2 = 1 if y[-1][-2] > 0 else -1
-            ysalida3 = 1 if y[-1][-1] > 0 else -1
+            ysalida1 = 1 if y[-1][3] > 0 else -1
+            ysalida2 = 1 if y[-1][2] > 0 else -1
+            ysalida3 = 1 if y[-1][1] > 0 else -1
     
             accur = (accur + 1) if (ysalida1 == ye[0] and ysalida2 == ye[1] and ysalida3 == ye[2]) else accur
    
             errorC += np.linalg.norm(ye - y[-1][1:])
-        
+         
         accurV[e] = accur/(np.size(trn[:,0]))
         errorCV[e] = errorC/(np.size(trn[:,0]))
+    accurVPar[i] = np.mean(accurV)
+    errorVPar[i] = np.mean(errorCV)
+
+plt.plot(range(cantPart), accurVPar, 'k')
+plt.show()
     
-    
-        print("accur: {}, errorC: {}".format(accurV[e], errorCV[e]))
+        #print("accur: {}, errorC: {}".format(accurV[e], errorCV[e]))
         #print(desempeño)
     #    if desempeño>tasa:
     #        break
