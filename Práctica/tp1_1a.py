afeec503be9053c93af46d186e601393bf94e2d1
shirli19 <shirli_cernotto@hotmail.com>
2019-09-12 18:40:42 -0300

    tp1_a

--- Práctica/tp1_1a.py
+++ Práctica/tp1_1a.py
@@ -8,14 +8,16 @@ This is a temporary script file.
 import numpy as np
 from matplotlib import pyplot as plt
 
-reader = np.genfromtxt("or_trn.csv", delimiter=',')
+plt.figure(1)
+reader = np.genfromtxt("xor_trn.csv", delimiter=',')
 
 w = np.random.uniform(-0.5,0.5,3)
-epoc = 10
+epoc = 100
+tasa = 0.69
 
 trn = np.append(-np.ones((len(reader[:,1]),1)),reader[:, 0:2],1)
 result = reader[:, 2]
-vel = 0.1
+vel = 0.01
 accurV=np.zeros((epoc,1))
 wV = np.zeros((epoc,3))
 
@@ -35,14 +37,17 @@ for i in range(epoc):
         y = 1 if z >= 0 else -1
         error=result[j] - y
         accur = (accur + 1 if error == 0 else accur)
-    accurV[i]=accur/len(trn[:,1])    
-                
 
-plt.scatter(trn[:,1],trn[:,2])
-plt.plot(trn[:,1],-trn[:,1]*w[1]/w[2]+w[0]/w[2], 'g')
-plt.show()
+    accurV[i]= accur/len(trn[:,1])    
+    plt.scatter(trn[:,1],trn[:,2])
+    plt.plot(trn[:,1],-trn[:,1]*w[1]/w[2]+w[0]/w[2], 'g')
+    print("Grafico {}".format(i))
+    plt.show()
+    if tasa < accurV[i]:
+        break                
 
-reader = np.genfromtxt("or_tst.csv", delimiter=',')
+
+reader = np.genfromtxt("xor_tst.csv", delimiter=',')
 trn = np.append(-np.ones((len(reader[:,1]),1)),reader[:, 0:2],1)
 result = reader[:, 2]
 accur = 0
@@ -52,5 +57,4 @@ for j in range(len(trn[:,0])):
     error=result[j] - y
     accur = (accur + 1 if error == 0 else accur)
 
-print(accur/len(trn[:,1]))    
-
+print(accur/len(trn[:,1]))  
